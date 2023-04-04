2abe12a897347bd863674d0ca390c9e49d035805
Sylvain Chevallier <sylain.chevallier@universite-paris-saclay.fr>
2023-04-04 19:26:12 +0200

    feat: plot reduced matrices, rename supervised rpca

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from pymanopt.optimizers.line_search import AdaptiveLineSearcher
 from pyriemann.datasets import make_matrices
 
-from riemannianpca import compute_subspaces
+from riemannianpca import compute_supervised_rpca
 
 rs = np.random.RandomState(42)
-n, n_dim, r_dim = 10, 5, 2
+n, n_dim, r_dim = 10, 10, 4
 C = make_matrices(2 * n, n_dim, "spd", rs)
 y = np.concatenate((np.zeros(n), np.ones(n)))
 solv_args = {
@@ -14,7 +15,8 @@ solv_args = {
     "max_time": float("inf"),
 }
 
-Uopt, logs = compute_subspaces(
+
+U, logs = compute_supervised_rpca(
     C=C,
     subspace_dim=r_dim,
     y=y,
@@ -25,3 +27,33 @@ Uopt, logs = compute_subspaces(
     init=None,
     k=3,
 )
+
+# Reduced matrices are :
+# U.T @ C[i] @ U
+
+# Plot
+plt.figure(figsize=(7, 7))
+for i in range(5):
+    ax = plt.subplot(2, 5, i + 1)
+    plt.imshow(C[i], cmap=plt.get_cmap("RdBu_r"))
+    plt.xticks([])
+    if i == 0:
+        plt.yticks(np.arange(n_dim))
+        ax.tick_params(axis="both", which="major", labelsize=7)
+    else:
+        plt.yticks([])
+    if i == 2:
+        plt.title("Cov original space")
+
+for i, pos in enumerate(range(5, 10)):
+    ax = plt.subplot(2, 5, pos + 1)
+    plt.imshow(U.T @ C[i] @ U, cmap=plt.get_cmap("RdBu_r"))
+    plt.xticks([])
+    if i == 0:
+        plt.yticks(np.arange(r_dim))
+        ax.tick_params(axis="both", which="major", labelsize=7)
+    else:
+        plt.yticks([])
+    if i == 2:
+        plt.title("Cov reduced space")
+plt.show()
